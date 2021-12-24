552161085ba56ba9e1d1f42786bb589cfa891786
Larsluph <remarso59@sfr.fr>
2021-12-24 12:21:54 +0100

    remove debug mode

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 """run flask web server."""
 
+import colorama
+
 from web_storage import app
 
+colorama.init()
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=49550)
+    app.run(debug=False, host='0.0.0.0', port=49550)
